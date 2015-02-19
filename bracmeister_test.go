4a6d96d9a9c8b80ab45d778bdff1b6f88412bad5
Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2015-02-19 18:46:45 +0900

    Modify testing: used testing package(had ever not use)

--- bracmeister_test.go
+++ bracmeister_test.go
@@ -1,30 +1,54 @@
 package bracmeister
 
-import (
-	"testing"
-
-	"github.com/k0kubun/pp"
-)
+import "testing"
 
 func TestSimple(t *testing.T) {
-	pp.Println("E", Calc(168, 88, 56, true))
-	pp.Println("E", Calc(168, 88, 56, false))
+	if "F" != Calc(168, 88, 56, true).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(168, 88, 56, true).Cup)
+	}
+	if "F" != Calc(168, 88, 56, false).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(168, 88, 56, false).Cup)
+	}
 
-	pp.Println("G", Calc(162, 90, 58, true))
-	pp.Println("G", Calc(162, 90, 58, false))
+	if "F" != Calc(162, 90, 58, true).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(162, 90, 58, true).Cup)
+	}
+	if "F" != Calc(162, 90, 58, false).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(162, 90, 58, false).Cup)
+	}
 
-	pp.Println("E", Calc(162, 88, 58, true))
-	pp.Println("E", Calc(162, 88, 58, false))
+	if "F" != Calc(162, 88, 58, true).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(162, 88, 58, true).Cup)
+	}
+	if "E" != Calc(162, 88, 58, false).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(162, 88, 58, false).Cup)
+	}
 
-	pp.Println("E", Calc(161, 86, 58, true))
-	pp.Println("E", Calc(161, 86, 58, false))
+	if "E" != Calc(161, 86, 58, true).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(161, 86, 58, true).Cup)
+	}
+	if "E" != Calc(161, 86, 58, false).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(161, 86, 58, false).Cup)
+	}
 
-	pp.Println("D", Calc(160, 84, 57, true))
-	pp.Println("D", Calc(160, 84, 57, false))
+	if "D" != Calc(160, 84, 57, true).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(160, 84, 57, true).Cup)
+	}
+	if "D" != Calc(160, 84, 57, false).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(160, 84, 57, false).Cup)
+	}
 
-	pp.Println("F", Calc(170, 92, 62, true))
-	pp.Println("F", Calc(170, 92, 62, false))
+	if "F" != Calc(170, 92, 62, true).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(170, 92, 62, true).Cup)
+	}
+	if "E" != Calc(170, 92, 62, false).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(170, 92, 62, false).Cup)
+	}
 
-	pp.Println("G", Calc(164, 93, 59, true))
-	pp.Println("G", Calc(164, 93, 59, false))
+	if "G" != Calc(164, 93, 59, true).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(164, 93, 59, true).Cup)
+	}
+	if "G" != Calc(164, 93, 59, false).Cup {
+		t.Errorf("Decided %s, As fuck", Calc(164, 93, 59, false).Cup)
+	}
 }
